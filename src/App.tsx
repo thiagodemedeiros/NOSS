94bb5e6afc04e19b7ef9e3fa50a71be1801ee0a5
Thiago Medeiros <thiagotitico1@gmail.com>
2026-05-25 21:48:10 -0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Layout } from "./Layout";
 import Home from "./feature/Home/Home";
 import { PagePartnerOrganizations } from "./feature/PartnerOrganizations/page/PagePartnerOrganizations";
+import { ScrollToTop } from "./components/ScrollToTop";
 
 export default function App() {
   return (
     <>
       <Router>
+        <ScrollToTop/>
         <Layout>
           <Routes>
               <Route path="/" element={<Home />} />
